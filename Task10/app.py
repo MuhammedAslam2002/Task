18ec32ca33db38012b8a0dda817646e2bc127c4c
Ubuntu <ubuntu@ip-172-31-24-168.ec2.internal>
2024-09-04 11:46:35 +0000

    first commit1

--- Task10/app.py
+++ Task10/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! hi'
+    return 'Hello, World! hi arshad'
 
 if __name__ == '__main__':
     app.run(debug=True)
