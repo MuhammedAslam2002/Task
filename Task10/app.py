40df08eedeac5804b93acf8fdaa71a6b2c101e2c
Ubuntu <ubuntu@ip-172-31-24-168.ec2.internal>
2024-09-05 04:58:40 +0000

    New one

--- Task10/app.py
+++ Task10/app.py
@@ -4,11 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-<<<<<<< HEAD
-    return 'Hello, World! hi aslam'
-=======
-    return 'Hello, World! hi guys'
->>>>>>> 230650601109e1465c70cf8c74628d516828b41e
+    return 'Hello, Aslam,Sherin,Arshad!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
